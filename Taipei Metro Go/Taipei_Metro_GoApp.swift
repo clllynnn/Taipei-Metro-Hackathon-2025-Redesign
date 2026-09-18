27bbf4536695467a7ae0da1dfb9f42c97a1fd50d
clllynnn <itslynn0927@gmail.com>
2026-09-18 12:58:04 +0800

    Initial Commit

---
+++ Taipei Metro Go/Taipei_Metro_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taipei_Metro_GoApp.swift
+//  Taipei Metro Go
+//
+//  Created by lynn on 2026/9/18.
+//
+
+import SwiftUI
+
+@main
+struct Taipei_Metro_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
